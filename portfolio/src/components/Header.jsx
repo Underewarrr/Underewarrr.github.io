0649fe22c3d8625d99cd4b1db288cb49c6130238
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-06-23 09:43:16 -0300

    Fix: Files structure

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Header>
+        <div>
+            <p>Portfólio</p>
+            <p>Minhas Habilidades</p>
+        </div>
+    </Header>
+  )
+}
+
+export default Header
